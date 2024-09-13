ccdeb0cbdfec8b232e857d1a11b1aab1e25cf268
Recep CAN <recepcn61@gmail.com>
2024-09-13 15:24:05 +0300

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>{
+console.log("mongodb connected")
+})
+.catch(err=>{
+    console.log("db error",err)
+})
+
 const app =express();
 
 app.listen(3000,()=>{
