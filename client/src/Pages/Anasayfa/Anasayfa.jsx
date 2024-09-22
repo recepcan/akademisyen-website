35c57b8a78a5596592239a5fafe23c97f03daada
Recep CAN <recepcn61@gmail.com>
2024-09-22 23:56:34 +0300

    anasayfa düzenlenmesi

--- client/src/Pages/Anasayfa/Anasayfa.jsx
+++ client/src/Pages/Anasayfa/Anasayfa.jsx
@@ -8,10 +8,13 @@ function Anasayfa() {
   
   return (
     <div className='min-h-screen  
-    flex flex-col items-center justify-center space-y-8  '>
+    flex flex-col items-center justify-center space-y-20  '>
 <ProfilCard/>
 
+<div className='w-full flex flex-col items-center justify-center'>
+<h1 className='text-5xl font-bold '>Blog</h1>
     <GetPosts limit={3}/>
+</div>
    {/* <Editor/>*/}
   
     </div>
