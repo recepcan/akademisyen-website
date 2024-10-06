7debfac72ca2d40859dadab69cb4b435a4f72ee2
Recep CAN <recepcn61@gmail.com>
2024-10-07 01:12:17 +0300

    u

--- api/Routes/authRoutes.js
+++ api/Routes/authRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { forgotPassword, google, resetPassword, signIn, signUp, signOut } from '../Controller/userAuthController.js';
+import { verifyToken } from '../utils/verifyToken.js';
 // import { verifyToken } from '../utils/verifyToken.js';
 // import { verifyAdmin } from '../utils/verifyAdmin.js';
 
 const router = express.Router();
-
 router.post('/sign-up', signUp)
 router.post('/reset-password/:id/:token',resetPassword)
 router.post('/forgot-password',forgotPassword)
